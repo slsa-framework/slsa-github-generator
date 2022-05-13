d7af8280bc22cc9d6c4e1eb94e9b17bce1fd890f
Ian Lewis <ianlewis@google.com>
2022-05-13 04:07:05 +0000

    Revert back to using github.workflow as entrypoint
    Updates #11

--- slsa/github_test.go
+++ slsa/github_test.go
@@ -8,15 +8,12 @@ import (
 
 func TestNewWorkflowRun(t *testing.T) {
 	t.Run("entrypoint", func(t *testing.T) {
-		workflowPath := ".github/workflows/workflow.yml"
-
 		r := NewWorkflowRun(nil, github.WorkflowContext{
-			Event: map[string]interface{}{
-				"workflow": workflowPath,
-			},
+			// TODO(github.com/slsa-framework/slsa-github-generator/issues/11): Use path to workflow file.
+			Workflow: "Workflow Name",
 		})
 
-		if want, got := workflowPath, r.Invocation.ConfigSource.EntryPoint; want != got {
+		if want, got := "Workflow Name", r.Invocation.ConfigSource.EntryPoint; want != got {
 			t.Errorf("unexpected entrypoint, want: %q, got: %q", want, got)
 		}
 	})
