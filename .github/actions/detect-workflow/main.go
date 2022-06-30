2ea1ac3953f67999c1d52da5e01d2ee60d0386d6
laurentsimon <64505099+laurentsimon@users.noreply.github.com>
2022-06-29 17:46:38 -0700

    feat: Action to checkout and setup Go env (#435)
    * Update doc
    
    * comments
    
    * Action for Go project checkout
    
    * Update doc
    
    * update
    
    * update
    
    * update
    
    * update
    
    * test
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update internal/builders/generic/README.md
    
    Co-authored-by: Ian Lewis <ianlewis@google.com>
    
    * comments
    
    Co-authored-by: Ian Lewis <ianlewis@google.com>

--- .github/actions/detect-workflow/main.go
+++ .github/actions/detect-workflow/main.go
@@ -150,6 +150,11 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Log to help troubleshooting.
+	fmt.Printf("repository:%s\n", repository)
+	fmt.Printf("ref:%s\n", ref)
+
+	// Output of the Action.
 	fmt.Println(fmt.Sprintf(`::set-output name=repository::%s`, repository))
 	fmt.Println(fmt.Sprintf(`::set-output name=ref::%s`, ref))
 }
