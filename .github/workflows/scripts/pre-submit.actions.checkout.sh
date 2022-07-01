2c50b509a7ee377b1895527ae98ec9e6cad56b37
laurentsimon <laurentsimon@google.com>
2022-07-01 19:19:06 +0000

    update

--- .github/workflows/scripts/pre-submit.actions.checkout.sh
+++ .github/workflows/scripts/pre-submit.actions.checkout.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Verify that no internal Actions are using `actions/checkout`
 # See reasoning in ./github/actions/README.md
 
-results=$(grep -R --include='*.yml' --include='*.yaml' -e 'actions/checkout@\|actions/checkout-go@' .github/actions/* | grep -v 'checkout-go\|generate-builder')
+results=$(grep -r --include='*.yml' --include='*.yaml' -e 'actions/checkout@\|actions/checkout-go@' .github/actions/* | grep -v 'checkout-go\|generate-builder')
 if [[ "$results" != "" ]]; then
     echo "Some Actions are using 'actions/checkout'"
     echo "$results"
