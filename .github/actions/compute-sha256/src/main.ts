05b8c3b712cc3348bbf5047e4dc380d205b91c60
naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
2022-07-19 08:59:38 -0500

    Included workflow to validate the computesha256
    - Included the workflow to validate the computesha256 github action
    
    Signed-off-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>

--- .github/actions/compute-sha256/src/main.ts
+++ .github/actions/compute-sha256/src/main.ts
@@ -1,21 +1,21 @@
-import * as core from '@actions/core'
-import * as fs from 'fs'
-import * as crypto from 'crypto'
+import * as core from "@actions/core";
+import * as fs from "fs";
+import * as crypto from "crypto";
 
 function shasum256(untrustedPath: string): string {
   if (!fs.existsSync(untrustedPath)) {
-    throw new Error(`File ${untrustedPath} not present`)
+    throw new Error(`File ${untrustedPath} not present`);
   }
-  const untrustedFile = fs.readFileSync(untrustedPath)
-  return crypto.createHash('sha256').update(untrustedFile).digest('hex')
+  const untrustedFile = fs.readFileSync(untrustedPath);
+  return crypto.createHash("sha256").update(untrustedFile).digest("hex");
 }
 
 async function run(): Promise<void> {
   // Get the path to the untrusted file from ENV variable 'UNTRUSTED_PATH'
-  const untrustedPath = core.getInput('untrusted_path')
-  core.info(`Computing sha256 of ${untrustedPath}`)
-  const sha = shasum256(untrustedPath)
-  core.info(`Computed sha256 of ${untrustedPath} as ${sha}`)
-  core.setOutput('sha256', sha)
+  const untrustedPath = core.getInput("untrusted_path");
+  core.info(`Computing sha256 of ${untrustedPath}`);
+  const sha = shasum256(untrustedPath);
+  core.info(`Computed sha256 of ${untrustedPath} as ${sha}`);
+  core.setOutput("sha256", sha);
 }
-run()
+run();
