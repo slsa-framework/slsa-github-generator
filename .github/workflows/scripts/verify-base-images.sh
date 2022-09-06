981267ce3ee6bfc55f4b99eee1becaff20e158cd
Ian Lewis <ianlewis@google.com>
2022-09-06 02:50:25 +0000

    Add description comment
    Signed-off-by: Ian Lewis <ianlewis@google.com>

--- .github/workflows/scripts/verify-base-images.sh
+++ .github/workflows/scripts/verify-base-images.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+# verify-base-images.sh verifies that base images used in Dockerfiles are
+# referenced by image digest and signed by their developers. It should be run at
+# the git repository's root directory.
+#
+# distroless images are verified with cosign using the distroless project's
+# public key available here:
+# https://github.com/GoogleContainerTools/distroless#how-do-i-verify-distroless-images
+#
+# All other images are assumed to be Docker official images that are signed
+# using Docker Content Trust (https://docs.docker.com/engine/security/trust/).
+# The public key for Docker official images in included in Docker releases by
+# default so no signers or keys need to be added.
+
 set -euo pipefail
 
 # NOTE: Use read to avoid whitespace issues.
 find . -name Dockerfile -print0 | while IFS= read -r -d '' f; do
     echo "Checking $f"
-    grep FROM "$f" | while IFS= read -r line; do
+    grep "^FROM " "$f" | while IFS= read -r line; do
         image_full=$(echo "$line" | awk '{ print $2 }')
         image_name=$(echo "$image_full" | cut -d '@' -f 1)
         image_sha=$(echo "$image_full" | cut -d '@' -f 2- | cut -d ':' -f 2-)
