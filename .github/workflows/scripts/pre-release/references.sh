777e558a5e3196db8abb693edc221189e2936b0f
Pedro Kaj Kjellerup Nacht <pnacht@google.com>
2022-11-03 22:16:49 +0000

    📖 Note @main should be removed in the future
    Only needed until generic container generator is released.
    
    Signed-off-by: Pedro Kaj Kjellerup Nacht <pnacht@google.com>

--- .github/workflows/scripts/pre-release/references.sh
+++ .github/workflows/scripts/pre-release/references.sh
@@ -36,6 +36,8 @@ if [[ "$results" != "" ]]; then
 fi
 
 # Verify documentation refers to the most recent release tag
+# TODO(https://github.com/slsa-framework/slsa-github-generator/milestone/3):
+#   Remove `@main` from allowed tags
 results=$(
     find . -name "*.md" -print0 \
     | xargs -0 grep -Pn "uses: slsa-framework/slsa-github-generator/.*@(?!<|main|$RELEASE_TAG)" \
