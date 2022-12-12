a74130fb3ec1aca689da2148e2b3b08d324a49c8
Asra Ali <asraa@google.com>
2022-12-12 09:23:17 -0700

    fix presubmit call
    Signed-off-by: Asra Ali <asraa@google.com>

--- .github/workflows/scripts/pre-submit.e2e.go.default.sh
+++ .github/workflows/scripts/pre-submit.e2e.go.default.sh
@@ -27,8 +27,8 @@ LDFLAGS=$(e2e_this_file | cut -d '.' -f4 | grep -v noldflags)
 e2e_verify_common_all "$ATTESTATION"
 
 # Verify Go specific environment vars are set
-e2e_verify_predicate_invocation_environment "$1" "os" "ubuntu22"
-e2e_verify_predicate_invocation_environment "$1" "arch" "X64"
+e2e_verify_predicate_invocation_environment "$ATTESTATION" "os" "ubuntu22"
+e2e_verify_predicate_invocation_environment "$ATTESTATION" "arch" "X64"
 
 # Verify the subject and build type
 e2e_verify_predicate_subject_name "$ATTESTATION" "$BINARY"
