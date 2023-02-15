94737812279165ff2f3d37899d4e2f3b570e29b7
Ian Lewis <ianlewis@google.com>
2023-02-15 06:01:25 +0000

    Add comment
    Signed-off-by: Ian Lewis <ianlewis@google.com>

--- .github/workflows/scripts/pre-submit.markdown/markdown-toc.sh
+++ .github/workflows/scripts/pre-submit.markdown/markdown-toc.sh
@@ -5,6 +5,9 @@
 
 set -euo pipefail
 
+# NOTE: Do not include issue templates since they contain Front Matter.
+# markdown-toc will update Front Matter even if there is no TOC in the file.
+# See: https://github.com/jonschlinkert/markdown-toc/issues/151
 MD_FILES=$(
     find . -name '*.md' -type f \
         -not -iwholename '*/.git/*' \
