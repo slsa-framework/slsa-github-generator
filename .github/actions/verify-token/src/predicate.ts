8536b041fb3dbd93f24af2465c0b70e4094e9225
Ian Lewis <ianlewis@google.com>
2023-02-17 01:31:15 +0000

    remove unused import
    Signed-off-by: Ian Lewis <ianlewis@google.com>

--- .github/actions/verify-token/src/predicate.ts
+++ .github/actions/verify-token/src/predicate.ts
@@ -10,7 +10,7 @@ WIHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import * as process from "process";
+
 import * as fs from "fs";
 
 import {
