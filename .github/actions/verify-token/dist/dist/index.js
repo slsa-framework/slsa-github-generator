63be127d844243f1458166190bbcefd56a1e1de0
Ian Lewis <ianlewis@google.com>
2023-02-22 13:43:00 +0900

    fix: public-good-instance.json workaround (#1670)
    Signed-off-by: Ian Lewis <ianlewis@google.com>

--- .github/actions/verify-token/dist/dist/index.js
+++ .github/actions/verify-token/dist/dist/index.js
@@ -1,7 +1,7 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 3109:
+/***/ 4822:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
@@ -51311,7 +51311,7 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module is referenced by other modules so it can't be inlined
-/******/ 	var __webpack_exports__ = __nccwpck_require__(3109);
+/******/ 	var __webpack_exports__ = __nccwpck_require__(4822);
 /******/ 	module.exports = __webpack_exports__;
 /******/ 	
 /******/ })()
