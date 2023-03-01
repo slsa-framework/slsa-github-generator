575cd508137a4227c7533cdec065ccfcd9822e84
asraa <asraa@google.com>
2023-03-01 14:00:35 -0600

    feat: add detect-workflow-js to supersede detect-workflow (#1706)
    * feat: add detect-workflow-js to supersede detect-workflow
    
    Signed-off-by: Asra Ali <asraa@google.com>
    
    Signed-off-by: Asra Ali <asraa@google.com>

---
+++ .github/actions/detect-workflow-js/src/main.ts
@@ -0,0 +1,66 @@
+import * as core from "@actions/core";
+import path from "path";
+import { detectWorkflowFromOIDC, detectWorkflowFromContext } from "./detect";
+
+async function run(): Promise<void> {
+  /* Test locally. Requires a GitHub token:
+        $ env INPUT_TOKEN="$(gh auth token)" \
+        GITHUB_RUN_ID="4303658979" \
+        GITHUB_REPOSITORY="project-oak/oak" \
+        nodejs ./dist/index.js
+    */
+  const token = core.getInput("token");
+
+  const repoName = process.env.GITHUB_REPOSITORY;
+  if (!repoName) {
+    core.setFailed("No repository detected.");
+    return;
+  }
+
+  // Set outputs.
+  let [repository, ref, workflow] = ["", "", ""];
+
+  try {
+    if (
+      process.env.ACTIONS_ID_TOKEN_REQUEST_URL &&
+      process.env.ACTIONS_ID_TOKEN_REQUEST_TOKEN
+    ) {
+      // Use the OIDC token when available.
+      const aud = path.join(repoName, "detect-workflow-js");
+      [repository, ref, workflow] = await detectWorkflowFromOIDC(aud);
+    } else {
+      // Otherwise, try to use the referenced workflows from the current workflow run.
+      core.info(
+        "Failed to retrieve OIDC token. This may be due to missing id-token: write permissions."
+      );
+      [repository, ref, workflow] = await detectWorkflowFromContext(
+        repoName,
+        token
+      );
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else {
+      core.setFailed(`Unexpected error: ${error}`);
+    }
+  }
+
+  if (!repository) {
+    core.setFailed("No repository detected.");
+    return;
+  }
+  if (!ref) {
+    core.setFailed("No ref detected.");
+    return;
+  }
+  if (!workflow) {
+    core.setFailed("No workflow detected.");
+    return;
+  }
+
+  core.setOutput("repository", repository);
+  core.setOutput("ref", ref);
+  core.setOutput("workflow", workflow);
+}
+run();
