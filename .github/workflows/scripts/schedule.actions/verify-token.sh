0d6671ee30df12df046e3b33d3402c1ed0f42732
Asra Ali <asraa@google.com>
2023-03-01 14:15:34 -0600

    fix: fix verify-token test for workflow
    Signed-off-by: Asra Ali <asraa@google.com>

--- .github/workflows/scripts/schedule.actions/verify-token.sh
+++ .github/workflows/scripts/schedule.actions/verify-token.sh
@@ -24,12 +24,14 @@ run_id=$(echo "$VERIFIED_TOKEN" | jq -r '.github.run_id')
 run_number=$(echo "$VERIFIED_TOKEN" | jq -r '.github.run_number')
 sha=$(echo "$VERIFIED_TOKEN" | jq -r '.github.sha')
 workflow=$(echo "$VERIFIED_TOKEN" | jq -r '.github.workflow')
+workflow_ref=$(echo "$VERIFIED_TOKEN" | jq -r '.github.workflow_ref')
+workflow_sha=$(echo "$VERIFIED_TOKEN" | jq -r '.github.workflow_sha')
 event_name=$(echo "$VERIFIED_TOKEN" | jq -r '.github.event_name')
 repository=$(echo "$VERIFIED_TOKEN" | jq -r '.github.repository')
-repository_owner=$(echo "$VERIFIED_TOKEN" | jq -r '.github.repository_owner')
+repository_owner_id=$(echo "$VERIFIED_TOKEN" | jq -r '.github.repository_owner_id')
 ref=$(echo "$VERIFIED_TOKEN" | jq -r '.github.ref')
 ref_type=$(echo "$VERIFIED_TOKEN" | jq -r '.github.ref_type')
-actor=$(echo "$VERIFIED_TOKEN" | jq -r '.github.actor')
+actor_id=$(echo "$VERIFIED_TOKEN" | jq -r '.github.actor_id')
 
 e2e_assert_eq "delegator_generic_slsa3.yml" "$audience"
 e2e_assert_eq "$GITHUB_RUN_ATTEMPT" "$run_attempt"
@@ -37,16 +39,18 @@ e2e_assert_eq "$GITHUB_RUN_NUMBER" "$run_number"
 e2e_assert_eq "$GITHUB_RUN_ID" "$run_id"
 e2e_assert_eq "$GITHUB_SHA" "$sha"
 e2e_assert_eq "$GITHUB_WORKFLOW" "$workflow"
+e2e_assert_eq "$GITHUB_WORKFLOW_REF" "$workflow_ref"
+e2e_assert_eq "$GITHUB_WORKFLOW_SHA" "$workflow_sha"
 e2e_assert_eq "ubuntu-latest" "$runner_label"
 e2e_assert_eq "true" "$rekor_log_public"
 e2e_assert_eq "./actions/build-artifacts-composite" "$action_path"
 e2e_assert_eq '{"name1":"value1","name2":"value2","private-repository":true}' "$inputs"
 e2e_assert_eq "$GITHUB_EVENT_NAME" "$event_name"
 e2e_assert_eq "$GITHUB_REPOSITORY" "$repository"
-e2e_assert_eq "$GITHUB_REPOSITORY_OWNER" "$repository_owner"
+e2e_assert_eq "$GITHUB_REPOSITORY_OWNER_ID" "$repository_owner_id"
 e2e_assert_eq "$GITHUB_REF" "$ref"
 e2e_assert_eq "$GITHUB_REF_TYPE" "$ref_type"
-e2e_assert_eq "$GITHUB_ACTOR" "$actor"
+e2e_assert_eq "$GITHUB_ACTOR_ID" "$actor_id"
 e2e_assert_eq "$TOOL_REPOSITORY" "$GITHUB_REPOSITORY"
 e2e_assert_eq "$TOOL_REF" "$GITHUB_REF"
 
