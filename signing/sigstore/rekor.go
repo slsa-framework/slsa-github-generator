ef48ccac184396a2d8ed1d14394963e5239d2bd7
Ian Lewis <ianlewis@google.com>
2023-04-19 04:27:04 +0000

    Move GetRekorPubs outside of loop
    Signed-off-by: Ian Lewis <ianlewis@google.com>

--- signing/sigstore/rekor.go
+++ signing/sigstore/rekor.go
@@ -77,13 +77,15 @@ func (r *Rekor) Upload(ctx context.Context, att signing.Attestation) (signing.Lo
 	if err != nil {
 		return nil, fmt.Errorf("retrieving log uuid by index: %w", err)
 	}
+
+	pubs, err := cosign.GetRekorPubs(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("getting rekor public keys: %w", err)
+	}
+
 	var uuid string
 	for ix, entry := range resp.Payload {
 		entry := entry
-		pubs, err := cosign.GetRekorPubs(ctx)
-		if err != nil {
-			return nil, fmt.Errorf("getting rekor public keys: %w", err)
-		}
 
 		if err := cosign.VerifyTLogEntryOffline(ctx, &entry, pubs); err != nil {
 			return nil, fmt.Errorf("validating log entry: %w", err)
