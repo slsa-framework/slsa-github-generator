d5aaa686ed28aa64e74edd2bef1fb7a31de880f6
Ian Lewis <ianlewis@google.com>
2023-04-19 09:21:19 +0900

    chore: pin module installs in tests (#2003)
    Fixes #1661
    
    ---------
    
    Signed-off-by: Ian Lewis <ianlewis@google.com>

--- .github/workflows/scripts/pre-submit.markdown/markdown-toc.sh
+++ .github/workflows/scripts/pre-submit.markdown/markdown-toc.sh
@@ -5,20 +5,6 @@
 
 set -euo pipefail
 
-# NOTE: Do not include issue templates since they contain Front Matter.
-# markdown-toc will update Front Matter even if there is no TOC in the file.
-# See: https://github.com/jonschlinkert/markdown-toc/issues/151
-MD_FILES=$(
-    find . -name '*.md' -type f \
-        -not -iwholename '*/.git/*' \
-        -not -iwholename '*/vendor/*' \
-        -not -iwholename '*/node_modules/*' \
-        -not -iwholename '*/.github/ISSUE_TEMPLATE/*'
-)
-for filename in ${MD_FILES}; do
-    markdown-toc --bullets="-" -i "${filename}"
-done
-
 if [ "$(GIT_PAGER="cat" git diff --ignore-space-at-eol | wc -l)" -gt "0" ]; then
     echo "Detected TOC changes.  See status below:"
     GIT_PAGER="cat" git diff
