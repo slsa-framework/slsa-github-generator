0b69e1c8ff26f04e4c1bd8fc34aceeb2c0de98e1
Ian Lewis <ianlewis@google.com>
2023-04-24 00:56:11 +0000

    Update dist
    Signed-off-by: Ian Lewis <ianlewis@google.com>

--- .github/actions/detect-workflow-js/dist/index.js
+++ .github/actions/detect-workflow-js/dist/index.js
@@ -84,8 +84,10 @@ function detectWorkflowFromContext(repoName, token) {
             return Promise.reject(Error(`No reusable workflows detected ${JSON.stringify(workflowData)}.`));
         }
         let [repository, ref, workflow] = ["", "", ""];
-        // If this is a pull request, then look for the repo and head SHA from the
-        // pull_request event value.
+        // If this is a pull request on the main repository
+        // (slsa-framework/slsa-github-generator), then look for the repo and head
+        // SHA from the pull_request event value. Pull requests on forks are not
+        // supported.
         if ((workflowData.event === "pull_request" ||
             workflowData.event === "merge_group") &&
             workflowData.repository.full_name === "slsa-framework/slsa-github-generator") {
