934435652996c02a6317092984312602dfaf2a21
Ian Lewis <ianlewis@google.com>
2023-05-24 03:13:35 +0900

    chore: Add license headers (#2157)
    Fixes #1393 
    
    Adds license headers to most files. Also adds a pre-submit check to make
    sure that license headers are present in files.
    
    A new `Makefile` target was added to add license headers to files that
    don't already have one using the
    [`autogen`](https://github.com/mbrukman/autogen) tool.
    ```shell
    make autogen
    ```
    
    ---------
    
    Signed-off-by: Ian Lewis <ianlewis@google.com>

--- .github/workflows/scripts/pre-submit.actions/references.sh
+++ .github/workflows/scripts/pre-submit.actions/references.sh
@@ -1,4 +1,18 @@
-#!/bin/bash
+#!/bin/bash -eu
+#
+# Copyright 2023 SLSA Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Verify that Action calls that are pinned (not locally called)
 # use the same referemnce as the relealse tag.
@@ -10,9 +24,9 @@ cd __THIS_REPO__
 
 # Verify our Actions are referenced at main in workflows.
 results=$(
-    find .github/workflows/ -maxdepth 1 -name '*.yaml' -o -name '*.yml' -type f -print0 \
-    | xargs -0 grep -P "slsa-framework/slsa-github-generator/.*@(?!main)" \
-    || true
+    find .github/workflows/ -maxdepth 1 -name '*.yaml' -o -name '*.yml' -type f -print0 |
+        xargs -0 grep -P "slsa-framework/slsa-github-generator/.*@(?!main)" ||
+        true
 )
 if [[ "$results" != "" ]]; then
     echo "Some Actions are not referenced at main"
@@ -23,8 +37,8 @@ fi
 # Verify our Actions are referenced at main in internal actions.
 results=$(
     find .github/actions/ -maxdepth 2 -name '*.yaml' -o -name '*.yml' -type f -print0 \
-    xargs -0 grep -P "slsa-framework/slsa-github-generator/.*@(?!main)" \
-    || true
+        xargs -0 grep -P "slsa-framework/slsa-github-generator/.*@(?!main)" ||
+        true
 )
 if [[ "$results" != "" ]]; then
     echo "Some Actions are not referenced at main in internal Actions"
@@ -34,14 +48,12 @@ fi
 
 # Verify our Actions are referenced at main in external actions.
 results=$(
-    find actions/ -maxdepth 3 -name '*.yaml' -o -name '*.yml' -type f -print0 \
-    | xargs -0 grep -Pn "slsa-framework/slsa-github-generator/.*@(?!main)" \
-    || true
+    find actions/ -maxdepth 3 -name '*.yaml' -o -name '*.yml' -type f -print0 |
+        xargs -0 grep -Pn "slsa-framework/slsa-github-generator/.*@(?!main)" ||
+        true
 )
 if [[ "$results" != "" ]]; then
     echo "Some Actions are not referenced at main in external Actions"
     echo "$results"
     exit 1
 fi
-
-
--- .github/workflows/scripts/pre-submit.apis/verify-safefs.sh
+++ .github/workflows/scripts/pre-submit.apis/verify-safefs.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
+#!/bin/bash -eu
+#
+# Copyright 2023 SLSA Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -euo pipefail
 
 # Verify that only safe file functions are used.
 results=$(
     find .github/actions/ actions/ -maxdepth 3 -name '*.ts' -not -path "*/tscommon/*" -not -path "*/__tests__/*" -type f -print0 |
-    xargs -0 grep -P '(import.*|require.*)"fs"' ||
-    true
+        xargs -0 grep -P '(import.*|require.*)"fs"' ||
+        true
 )
 if [[ "$results" != "" ]]; then
     echo "Some Actions are using unsafe file system functions"
