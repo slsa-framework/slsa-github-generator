d1a900e23fa1275a5672f299cae507c9a6332122
laurentsimon <laurentsimon@google.com>
2023-05-25 02:30:07 +0000

    update
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- .github/actions/verify-token/src/predicate1.ts
+++ .github/actions/verify-token/src/predicate1.ts
@@ -115,7 +115,7 @@ export async function createPredicate(
     };
     // Throw an error for now. We have no generators using v1.0 yet
     // and it's not supported in the slsa-verifier.
-    throw new Error("not supported: #2164, #2164");
+    throw new Error("not supported: #2164, #1555");
   } else {
     // NOTE: the workflow information is available in the internalParameters.GITHUB_WORKFLOW_REF.
     predicate.buildDefinition.externalParameters = {
