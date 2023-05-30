2be8bbe511faff7e3998bfc7f48fc26983703b9a
asraa <asraa@google.com>
2023-05-30 10:19:36 -0500

    fix: fix schedule action after container-based action/workflow rename (#2191)
    Fixes
    https://github.com/slsa-framework/slsa-github-generator/issues/2187
    
    Signed-off-by: Asra Ali <asraa@google.com>

--- .github/workflows/scripts/schedule.actions/verify-container_based-predicate.sh
+++ .github/workflows/scripts/schedule.actions/verify-container_based-predicate.sh
@@ -26,7 +26,7 @@ echo "PREDICATE_CONTENT: $PREDICATE_CONTENT"
 # Verify common predicate fields.
 e2e_verify_common_all_v1 "$PREDICATE_CONTENT"
 e2e_verify_predicate_v1_buildDefinition_buildType "$PREDICATE_CONTENT" "https://slsa.dev/container-based-build/v0.1?draft"
-e2e_verify_predicate_v1_runDetails_builder_id "$PREDICATE_CONTENT" "https://github.com/$GITHUB_REPOSITORY/.github/workflows/e2e.create-docker_based-predicate.schedule.yml@$GITHUB_REF"
+e2e_verify_predicate_v1_runDetails_builder_id "$PREDICATE_CONTENT" "https://github.com/$GITHUB_REPOSITORY/.github/workflows/e2e.create-container_based-predicate.schedule.yml@$GITHUB_REF"
 
 # Verify source
 e2e_verify_predicate_v1_buildDefinition_externalParameters_source "$PREDICATE_CONTENT" "{\"uri\":\"git+https://github.com/$GITHUB_REPOSITORY@$GITHUB_REF\",\"digest\":{\"sha1\":\"$GITHUB_SHA\"}}"
