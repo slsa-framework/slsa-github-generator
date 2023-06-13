3769ca86e2606ddd92811f0dd757aef97539b9db
Noah Elzner <78953604+enteraga6@users.noreply.github.com>
2023-06-12 17:35:08 -0700

    CD_PATH Parsing bug fix
    Signed-off-by: Noah Elzner <78953604+enteraga6@users.noreply.github.com>

--- internal/builders/bazel/build.sh
+++ internal/builders/bazel/build.sh
@@ -27,8 +27,18 @@ bazel build "${BUILD_FLAGS[@]}" "${BUILD_TARGETS[@]}"
 
 # Using target string, copy artifact to binaries dir
 for CURR_TARGET in "${BUILD_TARGETS[@]}"; do
-  CD_PATH=${CURR_TARGET%:*}
-  CD_PATH=${CD_PATH////}
+  # Take out the first two // in CURR_TARGET
+  # "//src/internal:fib" --> "src/internal:fib"
+  CD_PATH=$(echo $CURR_TARGET | cut -d'/' -f3-)
+  
+  # Removes field after and including the colon
+  # "src/internal:fib" --> "src/internal"
+  CD_PATH=$(echo $CD_PATH | cut -d':' -f1)
+  
+  # Removes everything up to and including the first colon
+  # "//src/internal:fib" --> "fib"
   BINARY_NAME=${CURR_TARGET#*:}
+  
+  #Copy the binary to artifact directory, binaries
   cp "bazel-bin/$CD_PATH/$BINARY_NAME" ./binaries
 done
