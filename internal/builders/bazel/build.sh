f5d9bd1007c88a49bfb848aaa5782d43a930d37f
Noah Elzner <78953604+enteraga6@users.noreply.github.com>
2023-06-21 14:59:20 -0700

    debug continue
    Signed-off-by: Noah Elzner <78953604+enteraga6@users.noreply.github.com>
    Signed-off-by: Noah Elzner <elzner@google.com>

--- internal/builders/bazel/build.sh
+++ internal/builders/bazel/build.sh
@@ -28,12 +28,15 @@ bazel build "${BUILD_FLAGS[@]}" "${BUILD_TARGETS[@]}"
 # Use associative array as a set to increase efficency in avoiding double copying the target
 declare -A FILES_SET
 
+# Allows the modifications from sub-shell loops
+export FILES_SET
+
 # Using target string, copy artifact to binaries dir
 for CURR_TARGET in "${BUILD_TARGETS[@]}"; do
 
   # Uses a Starlark expression to pass new line seperated list of files produced by targets into the set of files
   (bazel cquery --output=starlark --starlark:expr="'\n'.join([f.path for f in target.files.to_list()])" "$CURR_TARGET" 2>/dev/null) | 
-    while read file; do
+    while -r read file; do
       # Key value is target path, value we do not care about and is set to constant "1"
       FILES_SET["${file}"]="1"
   done
@@ -41,5 +44,6 @@ done
 
 # Copy set of unique targets to binaries, without !, would give values not keys
 for file in "${!FILES_SET[@]}"; do
+  echo "$file"
   cp "$file" ./binaries
 done
