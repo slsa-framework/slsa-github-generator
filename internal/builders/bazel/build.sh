e71b671502e2e2dfd9387d2082eb03370d385a1c
Noah Elzner <elzner@google.com>
2023-06-28 20:22:12 +0000

    Simplify and clean up runfiles logic
    Signed-off-by: Noah Elzner <elzner@google.com>

--- internal/builders/bazel/build.sh
+++ internal/builders/bazel/build.sh
@@ -16,181 +16,171 @@
 
 set -euo pipefail
 
+# TODO(Issue #2331): switch copy to binaries to a temp dir
 mkdir binaries
 
 # Transfer flags and targets to their respective arrays
 IFS=' ' read -r -a build_flags <<< "${FLAGS}"
 IFS=' ' read -r -a build_targets <<< "${TARGETS}"
 
-# debug
-cat WORKSPACE
-
+# If the targets includes Java targets, include Java build flag
+# and add Github Runner Java rule to WORKSPACE
 if [[ "${INCLUDES_JAVA}" ]]
 then
   build_flags+="--java_runtime_version=myjdk"
-  echo "$JAVA_HOME"
-  echo "$PWD"
 
   java_rule="local_java_repository(
     name = \"myjdk\",
     java_home = \"$JAVA_HOME\",
   )"
 
+  # Echo the configuration for the local Github Runner java into the root WORKSPACE file.
   echo "load(\"@bazel_tools//tools/jdk:local_java_repository.bzl\", \"local_java_repository\")" >> ./WORKSPACE
   echo "$java_rule" >> ./WORKSPACE
-
 fi
 
-# Debug
-cat WORKSPACE
+# Use associative array as a set to increase efficency in avoiding double copying the target.
+# Useful in generic glob patterns.
+declare -A targets_set
 
-# Build with respect to entire arrays of flags and targets
-#bazel build "${build_flags[@]}" "${build_targets[@]}"
+################################################
+#                                              #
+#              Build Target Set                #
+#                                              #
+################################################
 
-echo "start first loop"
-echo ""
-
-# Could this query be slow? Maybe add input-flag for generic glob patterns?
 for input in "${build_targets[@]}"; do
   echo "$input"
+  
+  # Using bazel query extracts all targets from a glob pattern.
+  # Thus we can change Java targets to their _deploy.jar target.
   for target in $(bazel query "$input"); do
     echo "$target"
+
+    # Check to see if the target is a Java target. If it is the output
+    # will be the Java target.
     output=$(bazel query "kind(java_binary, $target)" 2>/dev/null)
     if [[ -n "$output" ]]
     then
-      # target is a java target
+      # Build Java target to deploy.
       bazel build "${build_flags[@]}" "${target}_deploy.jar"
+      inputs_set["${target}_deploy.jar"]="1"
     else
-      # target is not a bazel target
+      # Build target regularly.
       bazel build "${build_flags[@]}" "$target"
+      targets_set["$target"]="1" # A set of unique targets
     fi
   done
 done
 
-# Use associative array as a set to increase efficency in avoiding double copying the target
-declare -A files_set
-declare -A targets_set
+################################################
+#                                              #
+#      Generate and Copy Needed Artifacts      #
+#                                              #
+################################################
 
-# Create folders for each artifact and include the runfiles in that dir
-if [[ "${NEEDS_RUNFILES}" ]]
-then
-  for curr_target in "${build_targets[@]}"; do
-    # Removes everything up to and including the first colon
-    # "//src/internal:fib" --> "fib"
-    binary_name=${curr_target#*:}
+for curr_target in "${!targets_set[@]}"; do
+  
+  # Removes everything up to and including the first colon
+  # "//src/internal:fib" --> "fib"
+  binary_name=${curr_target#*:}
+
+  ################################################
+  #                                              #
+  #            Logic for Java Targets            #
+  #                                              #
+  ################################################
+
+  # If the target name includes _deploy.jar it is a Java target.
+  if [[ "$binary_name" == *"_deploy.jar"* ]]
+  then
+    # Uses _deploy.jar as a field seperator and grabs the field before it.
     run_script_name=$(echo $binary_name | awk -F'_deploy.jar' '{print $1}')
-    echo "$run_script_name"
-    echo "$binary_name"
-    # Logic for Java Targets
-    if [[ "$binary_name" == *"_deploy.jar"* ]]
-    then
-      # Create dir for artifact and its runfiles
-      mkdir "./binaries/$run_script_name"
 
-      # Output is for binary_name.jar
-      bazel_generated=$(bazel cquery --output=starlark --starlark:expr="'\n'.join([f.path for f in target.files.to_list()])" "$curr_target" 2>/dev/null)
+    # Create dir for artifact and its runfiles
+    mkdir "./binaries/$run_script_name"
 
-      #TODO: dont need while here just one output from bazel cquery
-      # Uses a Starlark expression to pass new line seperated list of file(s) into the set of files
-      while read -r file; do
-        # Key value is target path, value we do not care about and is set to constant "1"
-        cp -Lr "$file" "./binaries/$run_script_name"
-
-        run_script_path=$(echo $file | awk -F'_deploy.jar' '{print $1}')
-
-        if [[ ! -z "${USER_LOCAL_JAVABIN}" ]]
-        then
-          # Configure runscript such that it will run on user's machine
-          # TODO: maybe ask Bazel if they can input a flag for this
-          # Or should there be a wrapper script around the wrapper script with the flag?
-          sed -i "46i JAVABIN=$USER_LOCAL_JAVABIN" "$run_script_path"
-        fi
-        cp -L "$run_script_path" "./binaries/$run_script_name"
-      done <<< "$bazel_generated"
-
-      #Add the artifact & runfile dir to set of files
-      #files_set["./binaries/$binary_name"]="1" #already added to binaries dont need to cp again
-
-    else # Logic for other targets needing runfiles
-
-      # Run cquery twice and transfer targets in case of generic glob patterns such as "//src/..." that intersect with other targets
-      bazel_generated=$(bazel cquery --output=starlark --starlark:expr="'\n'.join([f.path for f in target.files.to_list()])" "$curr_target" 2>/dev/null)
-
-      # Uses a Starlark expression to pass new line seperated list of targets into the set of targets
-      while read -r target; do
-        # Key value is target path, value we do not care about and is set to constant "1"
-        targets_set["${target}"]="1"
-      done <<< "$bazel_generated"
-    fi
-  done
-
-  # DEBUG
-  for target in "${!targets_set[@]}"; do
-    # echo out entire targets set
-    echo "$target"
-  done
-
-  echo ""
-  echo "targets above"
-else # Upload the artfiacts as standalone pieces, no folders
-
-  # Using target string, copy artifact(s) to binaries dir
-  for curr_target in "${build_targets[@]}"; do
-    # Get file(s) generated from build with respect to the target
+    # Get the absolute path to output of Java JAR artifact.
     bazel_generated=$(bazel cquery --output=starlark --starlark:expr="'\n'.join([f.path for f in target.files.to_list()])" "$curr_target" 2>/dev/null)
 
-    # Uses a Starlark expression to pass new line seperated list of file(s) into the set of files
-    while read -r file; do
-      # Key value is target path, value we do not care about and is set to constant "1"
-      files_set["${file}"]="1"
-    done <<< "$bazel_generated"
-  done
-
-fi
-
-# From runfile loops --> unique targets and runfiles to copy
-#TODO: replace checking without perm change
-set +u #enable nounset to check empty sets
-if [[ "${#targets_set[@]}" -ne 0  ]]
-then
-  set -u # disable now
-  for unique_target in "${!targets_set[@]}"; do
-    # Removes everything up to and including the last /
-    # "./binaries/bazel-out/k8-fastbuild/bin/src/fib --> "fib"
-    echo $unique_target
-    binary_name=$(basename "$unique_target")
-    echo $binary_name
-    # Create dir for artifact and its runfiles
-    mkdir "./binaries/$binary_name"
-
-    # Copy the target and runfiles
-    # target_path=$(bazel cquery --output=starlark --starlark:expr="'\n'.join([f.path for f in target.files.to_list()])" "$unique_target" 2>/dev/null)
-    # echo "$target_path"
-    # echo "^"
-    cp -L "$unique_target" "./binaries/$binary_name"
-
-    # if runfiles dir exists
-    if [[ -d "${unique_target}.runfiles" ]]
+    # Copy JAR to artifact-specific dir in ./binaries and remove symbolic links.
+    file="$bazel_generated"
+    cp -Lr "$file" "./binaries/$run_script_name"
+    
+    # TODO: Create wrapper script to take in any local JAVABIN on input, and not from user's Github Workflow.
+    # Get the path the to run-script associated with the {$curr_target}_deploy.jar
+    # If the user inputted the path to their local JAVABIN insert that into the run-script to define it.
+    # Inputting a local path to JAVABIN is needed or else run-script will not work as it points to Github Runner JAVABIN
+    run_script_path=$(echo $file | awk -F'_deploy.jar' '{print $1}')
+    if [[ ! -z "${USER_LOCAL_JAVABIN}" ]]
     then
-      target_runfiles="${unique_target}.runfiles"
-      cp -Lr "$target_runfiles" "./binaries/$binary_name"
-      cd "./binaries/$binary_name/$binary_name.runfiles/"
-
-      # Unneeded and can contain unwanted symbolic links
-      rm -rf _main/external
-      rm -rf MANIFEST
-      rm -rf _repo_mapping
-
-      # Go back to the old dir
-      cd -
+      # Insert user's JAVABIN as env var to define it at beginning of run-script.
+      sed -i "46i JAVABIN=$USER_LOCAL_JAVABIN" "$run_script_path"
     fi
-  done
-fi
+    
+    cp -L "$run_script_path" "./binaries/$run_script_name"
 
-# Copy set of unique targets to binaries. Without !, it would give values not keys
-# TODO(Issue #2331): switch copy to binaries to a temp dir
-for file in "${!files_set[@]}"; do
-  # Remove the symbolic link and copy
-  # think i need to check to see if file path is already in binaries because of potential double intersection with JARs
-  cp -Lr "$file" ./binaries
+
+  ################################################
+  #                                              #
+  #          Logic for Non-Java Targets          #
+  #                                              #
+  ################################################
+  
+  else
+
+    ################################################
+    #                                              #
+    #       Logic for Runfile-Needing Targets      #
+    #                                              #
+    ################################################
+
+    if [[ "${NEEDS_RUNFILES}" ]]
+    then
+      # Get file(s) generated from build with respect to the target
+      bazel_generated=$(bazel cquery --output=starlark --starlark:expr="'\n'.join([f.path for f in target.files.to_list()])" "$curr_target" 2>/dev/null)
+
+      # Create dir for artifact and its runfiles
+      mkdir "./binaries/$binary_name"
+
+      # Uses a Starlark expression to pass new line seperated list of file(s) into the set of files
+      while read -r path_to_artifact do
+
+        # Copy generated artifact from absolute path from bazel cquery
+        cp -L "$path_to_artifact" "./binaries/$binary_name"
+
+        # if runfiles dir exists, copy runfiles into artifact's dir
+        if [[ -d "${path_to_artifact}.runfiles" ]]
+        then
+          path_to_target_runfiles="${path_to_artifact}.runfiles"
+          cp -Lr "$path_to_target_runfiles" "./binaries/$binary_name"
+          cd "./binaries/$binary_name/$binary_name.runfiles/"
+
+          # Unneeded and can contain unwanted symbolic links
+          rm -rf _main/external
+          rm -rf MANIFEST
+          rm -rf _repo_mapping
+
+          # Go back to the old dir
+          cd -
+        fi
+      done <<< "$bazel_generated"
+
+      
+    ################################################
+    #                                              #
+    #   Logic for NON-Runfile-Needing Targets      #
+    #                                              #
+    ################################################
+    else
+      # Get file(s) generated from build with respect to the target
+      bazel_generated=$(bazel cquery --output=starlark --starlark:expr="'\n'.join([f.path for f in target.files.to_list()])" "$curr_target" 2>/dev/null)
+      
+      # Uses a Starlark expression to pass new line seperated list of file(s) into the set of files
+      while read -r file; do
+          cp -L "$file" ./binaries
+      done <<< "$bazel_generated"
+    fi
+  
+  fi
 done
