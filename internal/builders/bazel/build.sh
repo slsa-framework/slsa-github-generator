8a2e8b17f432ad669642ddbbf74a1d3b199b43c5
Noah Elzner <78953604+enteraga6@users.noreply.github.com>
2023-06-30 08:56:24 -0700

    env var assignment wrong place
    Signed-off-by: Noah Elzner <78953604+enteraga6@users.noreply.github.com>
    Signed-off-by: Noah Elzner <elzner@google.com>

--- internal/builders/bazel/build.sh
+++ internal/builders/bazel/build.sh
@@ -116,8 +116,10 @@ for curr_target in "${!targets_set[@]}"; do
     # to run the run-script themselves, which would not be possible as it is either set to the Github Runner VM Java bin path
     # if no flag to USER_LOCAL_JAVABIN is passed in their workflow or to the path passed in their flag.
     awk -v n=66 -v s='    --local_javabin=*) USER_JAVA_BIN="${1#--local_javabin}" ;;' 'NR == n {print s} {print}' "$run_script_path" > temp_file && mv -f temp_file "$run_script_path"
-    awk -v n=82 -v s='' 'NR == n {print s} {print}' "$run_script_path" > temp_file && mv -f temp_file "$run_script_path"
-    awk -v n=83 -v s='if [[ -n $USER_JAVA_BIN ]]; then JAVABIN=$USER_JAVA_BIN; fi' 'NR == n {print s} {print}' "$run_script_path" > temp_file && mv -f temp_file "$run_script_path"
+    
+    # Updates Java Bin in run-script after the flags get proccessed
+    awk -v n=127 -v s='' 'NR == n {print s} {print}' "$run_script_path" > temp_file && mv -f temp_file "$run_script_path"
+    awk -v n=128 -v s='if [[ -n $USER_JAVA_BIN ]]; then JAVABIN=$USER_JAVA_BIN; fi' 'NR == n {print s} {print}' "$run_script_path" > temp_file && mv -f temp_file "$run_script_path"
     
     cp -L "$run_script_path" "./binaries/$run_script_name"
 
