3b817338f5887ccf509c74eb1d29d0b814198ca5
Noah Elzner <elzner@google.com>
2023-07-18 17:21:38 +0000

    Added temp directory for binaries to avoid collisions
    Signed-off-by: Noah Elzner <elzner@google.com>

--- internal/builders/bazel/build.sh
+++ internal/builders/bazel/build.sh
@@ -17,7 +17,7 @@
 set -euo pipefail
 
 # TODO(Issue #2331): switch copy to binaries to a temp dir
-mkdir binaries
+mkdir /tmp/bazel_builder_binaries_to_upload
 
 # Transfer flags and targets to their respective arrays
 IFS=' ' read -r -a build_flags <<< "${FLAGS}"
@@ -50,7 +50,7 @@ declare -A targets_set
 ################################################
 
 for input in "${build_targets[@]}"; do
-  
+
   # Using bazel query extracts all targets from a glob pattern.
   # Thus we can change Java targets to their _deploy.jar target.
   for target in $(bazel query "$input"); do
@@ -58,12 +58,12 @@ for input in "${build_targets[@]}"; do
     # Check to see if the target is a Java target. If it is the output is a Java target.
     # Note: targets that already have the _deploy.jar suffix will have no output from the query
     output=$(bazel query "kind(java_binary, $target)" 2>/dev/null)
-    
+
     # If there is a Java target without _deploy.jar suffix, add suffix, build and add to target set.
     if [[ -n "$output" ]]
     then
       bazel build "${build_flags[@]}" "${target}_deploy.jar"
-      targets_set["${target}_deploy.jar"]="1"    
+      targets_set["${target}_deploy.jar"]="1"
     else
       # Build target regularly.
       bazel build "${build_flags[@]}" "$target"
@@ -79,7 +79,7 @@ done
 ################################################
 
 for curr_target in "${!targets_set[@]}"; do
-  
+
   # Removes everything up to and including the first colon
   # "//src/internal:fib" --> "fib"
   binary_name=${curr_target#*:}
@@ -97,15 +97,15 @@ for curr_target in "${!targets_set[@]}"; do
     run_script_name=$(echo "$binary_name" | awk -F'_deploy.jar' '{print $1}')
 
     # Create dir for artifact and its runfiles
-    mkdir "./binaries/$run_script_name"
+    mkdir "/tmp/bazel_builder_binaries_to_upload/$run_script_name"
 
     # Get the absolute path to output of Java JAR artifact.
     bazel_generated=$(bazel cquery --output=starlark --starlark:expr="'\n'.join([f.path for f in target.files.to_list()])" "$curr_target" 2>/dev/null)
 
     # Copy JAR to artifact-specific dir in ./binaries and remove symbolic links.
     file="$bazel_generated"
-    cp -Lr "$file" "./binaries/$run_script_name"
-    
+    cp -Lr "$file" "/tmp/bazel_builder_binaries_to_upload/$run_script_name"
+
     # Get the path the to run-script associated with the {$curr_target}_deploy.jar
     # If the user inputted the path to their local JAVABIN insert that into the run-script to define it.
     # Inputting a local path to JAVABIN is needed or else run-script will not work as it points to Github Runner JAVABIN
@@ -116,11 +116,11 @@ for curr_target in "${!targets_set[@]}"; do
     # to run the run-script themselves, which would not be possible as it is either set to the Github Runner VM Java bin path
     # if no flag to USER_LOCAL_JAVABIN is passed in their workflow or to the path passed in their flag.
     awk -v n=66 -v s='    --local_javabin=*) USER_JAVA_BIN=( "${1#--local_javabin=}" ) ;;' 'NR == n {print s} {print}' "$run_script_path" > temp_file && mv -f temp_file "$run_script_path"
-    
+
     # Updates Java Bin in run-script after the flags get proccessed
     awk -v n=127 -v s='' 'NR == n {print s} {print}' "$run_script_path" > temp_file && mv -f temp_file "$run_script_path"
     awk -v n=128 -v s='if [[ -n $USER_JAVA_BIN ]]; then JAVABIN=$USER_JAVA_BIN; fi' 'NR == n {print s} {print}' "$run_script_path" > temp_file && mv -f temp_file "$run_script_path"
-    
+
     cp -L "$run_script_path" "./binaries/$run_script_name"
 
   ################################################
@@ -128,7 +128,7 @@ for curr_target in "${!targets_set[@]}"; do
   #          Logic for Non-Java Targets          #
   #                                              #
   ################################################
-  
+
   else
 
     ################################################
@@ -143,20 +143,20 @@ for curr_target in "${!targets_set[@]}"; do
       bazel_generated=$(bazel cquery --output=starlark --starlark:expr="'\n'.join([f.path for f in target.files.to_list()])" "$curr_target" 2>/dev/null)
 
       # Create dir for artifact and its runfiles
-      mkdir "./binaries/$binary_name"
+      mkdir "/tmp/bazel_builder_binaries_to_upload/$binary_name"
 
       # Uses a Starlark expression to pass new line seperated list of file(s) into the set of files
       while read -r path_to_artifact; do
 
         # Copy generated artifact from absolute path from bazel cquery
-        cp -L "$path_to_artifact" "./binaries/$binary_name"
+        cp -L "$path_to_artifact" "/tmp/bazel_builder_binaries_to_upload/$binary_name"
 
         # if runfiles dir exists, copy runfiles into artifact's dir
         if [[ -d "${path_to_artifact}.runfiles" ]]
         then
           path_to_target_runfiles="${path_to_artifact}.runfiles"
-          cp -Lr "$path_to_target_runfiles" "./binaries/$binary_name"
-          cd "./binaries/$binary_name/$binary_name.runfiles/"
+          cp -Lr "$path_to_target_runfiles" "/tmp/bazel_builder_binaries_to_upload/$binary_name"
+          cd "/tmp/bazel_builder_binaries_to_upload/$binary_name/$binary_name.runfiles/"
 
           # Unneeded and can contain unwanted symbolic links
           rm -rf _main/external
@@ -176,10 +176,10 @@ for curr_target in "${!targets_set[@]}"; do
     else
       # Get file(s) generated from build with respect to the target
       bazel_generated=$(bazel cquery --output=starlark --starlark:expr="'\n'.join([f.path for f in target.files.to_list()])" "$curr_target" 2>/dev/null)
-      
+
       # Uses a Starlark expression to pass new line seperated list of file(s) into the set of files
       while read -r file; do
-          cp -L "$file" ./binaries
+          cp -L "$file" /tmp/bazel_builder_binaries_to_upload
       done <<< "$bazel_generated"
     fi
   fi
