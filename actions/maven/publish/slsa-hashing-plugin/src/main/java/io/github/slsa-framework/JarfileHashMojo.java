d08ce2b75236fc166d9ac69cc2bec94a90073603
AdamKorcz <adam@adalogics.com>
2023-08-01 21:38:56 +0100

    remove hyphen in java package name
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- actions/maven/publish/slsa-hashing-plugin/src/main/java/io/github/slsa-framework/JarfileHashMojo.java
+++ actions/maven/publish/slsa-hashing-plugin/src/main/java/io/github/slsa-framework/JarfileHashMojo.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package io.github.slsa-framework.slsa-github-generator;
+package io.github.slsaframework;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
