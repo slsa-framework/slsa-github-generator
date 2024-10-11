8c489a2879f536f5e84cb2ff3c585a282bad021f
Ramon Petgrave <ramon.petgrave64@gmail.com>
2024-10-11 05:15:41 -0400

    lint: fix copyloopvar
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- internal/utils/marshal_test.go
+++ internal/utils/marshal_test.go
@@ -46,7 +46,6 @@ func Test_MarshalToString(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt // Re-initializing variable so it is not changed while executing the closure below
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
@@ -91,7 +90,6 @@ func Test_UnmarshalList(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		tt := tt // Re-initializing variable so it is not changed while executing the closure below
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
