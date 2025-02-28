49bd23b03e9b16cc07c0983010fcd811b64fe7cb
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-02-28 22:00:50 +0900

    fix: fix the Go module path
    Signed-off-by: Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>

--- internal/builders/container/generate_test.go
+++ internal/builders/container/generate_test.go
@@ -21,8 +21,8 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/slsa-framework/slsa-github-generator/internal/utils"
-	"github.com/slsa-framework/slsa-github-generator/slsa"
+	"github.com/slsa-framework/slsa-github-generator/v2/internal/utils"
+	"github.com/slsa-framework/slsa-github-generator/v2/slsa"
 )
 
 func checkTest(t *testing.T) func(err error) {
